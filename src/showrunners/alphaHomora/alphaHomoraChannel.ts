64726e5ecd59d3c7cf8c866ccff3d03937b97bc3
akp111 <pradhanashish95@gmail.com>
2021-08-30 16:33:54 +0530

    cta added

--- src/showrunners/alphaHomora/alphaHomoraChannel.ts
+++ src/showrunners/alphaHomora/alphaHomoraChannel.ts
@@ -6,7 +6,7 @@ import config from '../../config';
 import showrunnersHelper from '../../helpers/showrunnersHelper';
 // import PQueue from 'p-queue';
 import { ethers, logger } from 'ethers';
-import epnsHelper, {InfuraSettings, NetWorkSettings, EPNSSettings} from '@epnsproject/backend-sdk'
+import epnsHelper, { InfuraSettings, NetWorkSettings, EPNSSettings } from '@epnsproject/backend-sdk'
 // const queue = new PQueue();
 
 const infuraSettings: InfuraSettings = {
@@ -31,7 +31,7 @@ const NETWORK_TO_MONITOR = config.web3MainnetNetwork
 export default class AlphaHomoraChannel {
   constructor(
     @Inject('logger') private logger,
-  ) {}
+  ) { }
 
   public async getWalletKey() {
     var path = require('path');
@@ -51,14 +51,22 @@ export default class AlphaHomoraChannel {
     const AlphaHomoraContract = await sdk.getContract(alphaHomoraSettings.homoraBankDeployedContract, homoraBankDeployedContractABI)
     let next_pos = await AlphaHomoraContract.contract.functions.nextPositionId()
     next_pos = Number(next_pos.toString())
-    logger.info({next_pos})
-    await this.processDebtRatio(users, 100, AlphaHomoraContract.contract, sdk, simulate);
+    logger.info({ next_pos })
+    for (let i = 1; i < next_pos; i++) {
+      console.log(i)
+      if (i != 6)
+        await this.processDebtRatio(users, i, AlphaHomoraContract.contract, simulate);
+    }
   }
 
-  public async processDebtRatio(users: Array<string>, id: number, contract, sdk, simulate: boolean | Object) {
+  public async processDebtRatio(users: Array<string>, id: number, contract, simulate: boolean | Object) {
+    const walletKey = await this.getWalletKey()
+    const sdk = new epnsHelper(config.web3MainnetNetwork, walletKey, settings, epnsSettings);
+    const epns = sdk.advanced.getInteractableContracts(epnsSettings.network, settings, walletKey, epnsSettings.contractAddress, epnsSettings.contractABI)
+
     const position = await contract.functions.getPositionInfo(id)
     logger.info({ position: position.owner })
-    if (users.includes(position.owner)) {}
+    if (users.includes(position.owner)) { }
     let [borrowCredit, collateralCredit] = await Promise.all([contract.functions.getBorrowETHValue(id), contract.functions.getCollateralETHValue(id)]);
     borrowCredit = Number(ethers.utils.formatEther(borrowCredit[0]))
     collateralCredit = Number(ethers.utils.formatEther(collateralCredit[0]))
@@ -66,15 +74,18 @@ export default class AlphaHomoraChannel {
     logger.info({ debtRatio })
     if (debtRatio > Number(alphaHomoraSettings.homoraDebtRatioThreshold)) {
       const notificationType = 3;
-      const tx = await sdk.sendNotification(
-        position.owner,
-        'Position Liquidation',
-        `Your position of id: ${id} is at ${alphaHomoraSettings.homoraDebtRatioThreshold}% debt ratio and is at risk of liquidation`,
-        'Position Liquidation',
-        `Your position of id: ${id} is at ${alphaHomoraSettings.homoraDebtRatioThreshold}% debt ratio and is at risk of liquidation. [timestamp: ${Math.floor(new Date() / 1000)}]`,
-        notificationType,
-        simulate
-      )
+      const title = `Position Liquidation`
+      const message = `Your position of id: ${id} is at ${debtRatio}% debt ratio and is at risk of liquidation`
+      const payloadTitle = `Position Liquidation`;
+      const payloadMsg = `Your position of id: ${id} is at ${debtRatio}% debt ratio and is at risk of liquidation. [timestamp: ${Math.floor(new Date() / 1000)}]`;
+      const user: any = position.owner;
+      const cta: any = `https://alphafinance.io/`
+      const storageType = 1;
+      const trxConfirmWait = 0;
+      const payload = await sdk.advanced.preparePayload(user, notificationType, title, message, payloadTitle, payloadMsg, cta, null)
+      const ipfsHash = await sdk.advanced.uploadToIPFS(payload, logger, null, simulate)
+      const tx = await sdk.advanced.sendNotification(epns.signingContract, user, notificationType, storageType, ipfsHash, trxConfirmWait, logger, simulate)
+
       logger.info(`[${new Date(Date.now())}]-[Alpha Homora]- transaction: %o`, tx)
     }
   }
